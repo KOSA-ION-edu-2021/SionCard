8728431f61c3f6cbe9ff1b1bf210ff7bc9f1239a
YouHyunSoo <xowkd31226@gmail.com>
2021-10-25 11:05:47 +0900

    develop
    유현수

---
+++ card-pro/src/CardImf.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+      왜 안되는고야야ㅑ야야야아아아아아
+
+  </div>
+</template>
+
+<script>
+// import Bar from './components/Bar.vue'
+export default {
+    components: {
+        // Bar
+    }
+}
+</script>
+
+<style>
+
+</style>
