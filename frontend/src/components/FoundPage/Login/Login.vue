7d877160f165215d4375c08989a4ade7dfe508a0
SangOk <sangok1993@naver.com>
2021-10-28 13:26:01 +0900

    add_login

--- frontend/src/components/FoundPage/Login/Login.vue
+++ frontend/src/components/FoundPage/Login/Login.vue
@@ -1,37 +1,30 @@
 <template>
-  <v-container height="100%" class="pa-0 pt-10 red">
-    <v-row class="ma-auto pa-auto">
-        <v-col cols="5" class="pa-0 blue">
-            내용
-        </v-col>
-        <v-col cols="7" class="pa-0 blue">
-            <v-card class="ma-auto d-flex flex-column" width="330" height="300" tile flat>
-                <div class="pt-16">
-                    
-                    <v-text-field
-                        class="justify-start ma-0"
-                        label="아이디를 입력해 주세요."
-                        dense
-                        outlined
-                    ></v-text-field>
-                    <v-text-field
-                        class="justify-start ma-0"
-                        label="비밀번호를 입력해 주세요."
-                        dense
-                        hint="잘못 입력하셨습니다."
-                        
-                        outlined
-                    ></v-text-field>
-                </div>
-            </v-card>
-        </v-col>
-    </v-row>
-  </v-container>
+    <v-card
+        tile flat
+        style="margin-top:166px"
+    >
+        <v-row class="ma-auto pa-auto">
+            <v-col cols="1" md="2"/>
+            <v-col cols="3" class="pa-0 d-none d-md-block">
+                <LoginDeco/>
+            </v-col>
+            <v-col cols="10" md="6" class="pa-0">
+                <LoginForm/>
+            </v-col>
+            <v-col cols="1"/>
+        </v-row>
+    </v-card>
 </template>
 
 <script>
-export default {};
-</script>
+import LoginForm from "./LoginForm.vue"
+import LoginDeco from "./LoginDeco.vue"
 
-<style>
-</style>
+export default {
+
+    components:{
+        LoginForm,
+        LoginDeco
+    }
+};
+</script>
