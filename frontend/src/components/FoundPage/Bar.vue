050cfe9a7b2f8d7b8ae689e05bc3575074976e15
Jihoon-Yoon96 <03470@naver.com>
2021-11-09 12:30:57 +0900

    GNB 수정 + My-내 정보 수정

--- frontend/src/components/FoundPage/Bar.vue
+++ frontend/src/components/FoundPage/Bar.vue
@@ -1,110 +1,89 @@
 <template>
-  <v-app-bar app dark height="100px">
-    <!-- SION Card logo image -->
-    <router-link to="/">
-      <v-img
-        max-height="48px"
-        max-width="60px"
-        contain
-        src="@/assets/logo_transparent.png"
-      />
-    </router-link>
-    <router-link to="/" tag="button">
-    <v-col cols="auto" class="text-h5">
-      SionCard
-    </v-col>
-    </router-link>
+  
+      <v-app-bar app dark height="80px" class="pr-16">
+      <!-- SION Card logo image -->
+      <router-link to="/" class="ml-16">
+        <v-img
+          class="ml-16"
+          max-height="48px"
+          max-width="60px"
+          contain
+          src="@/assets/logo_transparent.png"
+        />
+      </router-link>
+      <router-link to="/" tag="button">
+      <v-col cols="auto" class="text-h5">
+        SionCard
+      </v-col>
+      </router-link>
+      
+      <v-spacer></v-spacer>
+
+      <!-- My menu -->
+      <v-menu offset-y open-on-hover>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            plain
+            v-bind="attrs"
+            v-on="on"
+            to="/my_main"
+          >
+            My
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item class="" to="/myinfo">
+            <v-list-item-title>
+                <!-- <router-link class="text-decoration-none" to="/myinfo"> -->
+                  내 정보
+                <!-- </router-link> -->
+              </v-list-item-title>
+          </v-list-item>
+          <v-list-item class="" to="/mycard">
+            <v-list-item-title>
+                <!-- <router-link class="text-decoration-none" to="/mycard"> -->
+                  내 카드
+                <!-- </router-link> -->
+              </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <!-- Card menu -->
+      <v-menu  offset-y open-on-hover >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            plain
+            v-bind="attrs"
+            v-on="on"
+            to="/card_main"
+            class="mr-16"
+          >
+            Card 
+          </v-btn>
+        </template>
+        <v-list >
+          <v-list-item class="" to="/cardinfor">
+            <v-list-item-title>
+              <!-- <router-link  to="/cardinfor" class="text-decoration-none"> -->
+                카드 안내
+              <!-- </router-link> -->
+              </v-list-item-title>
+          </v-list-item>
+          <v-list-item class="" to="/cardrecommend">
+            <!-- <router-link  to="/cardrecommend" class="text-decoration-none"> -->
+            <v-list-item-title >
+                카드 추천
+              </v-list-item-title>
+              <!-- </router-link> -->
+          </v-list-item>
+        </v-list>
+      </v-menu>    
+
+      
+
+    </v-app-bar>
     
-    <v-spacer></v-spacer>
-
-    <!-- My menu -->
-    <v-menu offset-y>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          plain
-          v-bind="attrs"
-          v-on="on"
-        >
-          My
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item color="white" to="/my_main">
-          <v-list-item-title >
-              <!-- <router-link class="text-decoration-none" to="/my_main"> -->
-                <div style="color:black">메인(임시 테스트용)</div>
-              <!-- </router-link> -->
-            </v-list-item-title>
-        </v-list-item>
-        <v-list-item class="" to="/myinfo">
-          <v-list-item-title>
-              <!-- <router-link class="text-decoration-none" to="/myinfo"> -->
-                내 정보
-              <!-- </router-link> -->
-            </v-list-item-title>
-        </v-list-item>
-        <v-list-item class="" to="/mycard">
-          <v-list-item-title>
-              <!-- <router-link class="text-decoration-none" to="/mycard"> -->
-                내 카드
-              <!-- </router-link> -->
-            </v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-
-    <!-- Card menu -->
-    <v-menu  offset-y>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          plain
-          v-bind="attrs"
-          v-on="on"
-        >
-          Card 
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item class="" to="/cardinfor">
-          <v-list-item-title>
-            <!-- <router-link  to="/cardinfor" class="text-decoration-none"> -->
-              카드 안내
-            <!-- </router-link> -->
-            </v-list-item-title>
-        </v-list-item>
-        <v-list-item class="" to="/cardrecommend">
-          <!-- <router-link  to="/cardrecommend" class="text-decoration-none"> -->
-          <v-list-item-title>
-              카드 추천
-            </v-list-item-title>
-            <!-- </router-link> -->
-        </v-list-item>
-      </v-list>
-    </v-menu>
-
-    <router-link
-      to="/card_main"
-      tag="div"
-    >
-      <v-btn
-        plain
-      > 
-      card_main
-      </v-btn>
-    </router-link>
-    <!-- Log In -->
-    <router-link
-      to="/login"
-      tag="div"
-    >
-      <v-btn
-        plain
-      > 
-      Log In
-      </v-btn>
-    </router-link>
-    
-  </v-app-bar>
 </template>
 
 <script>
