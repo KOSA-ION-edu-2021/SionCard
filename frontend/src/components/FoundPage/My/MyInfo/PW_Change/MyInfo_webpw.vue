69cf1d80052396745c2db4228365fb9d633629fe
Jihoon-Yoon96 <03470@naver.com>
2021-11-18 10:24:04 +0900

    MY 폴더 정리

---
+++ frontend/src/components/FoundPage/My/MyInfo/PW_Change/MyInfo_webpw.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-container>
+        <v-row class="mt-5">
+          <v-col class="text-h4 font-weight-bold">
+            본인 인증
+          </v-col>
+        </v-row>
+        <pw_table/>
+        
+    </v-container>
+</template>
+
+<script>
+import pw_table from './webpw_table.vue'
+
+export default {
+    name: "myinfo_webpw",
+    data: () => ({
+      radioValues: 'email',
+    }),
+    components:{
+      pw_table
+    },
+    methods:{
+      emailCheck(){
+        this.radioValues = 'email'
+      },
+      cardCheck(){
+        this.radioValues = 'card'
+      }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
